7a17417f01406a62e3ff7596f79b9c9ba2903598
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-10-14 14:31:10 +0300

    adding other files

---
+++ RickAndMortyApp/Services/AuthManager.swift
@@ -0,0 +1,17 @@
+//
+//  AuthManager.swift
+//  RickAndMortyApp
+//
+//  Created by MikhaiL on 09.10.2023.
+//
+
+import Foundation
+
+final class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    // Keychain реализация
+}
