aa29dee45175419cd209b9c99eda656ede1f8a3f
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-10-14 14:26:54 +0300

    create data model

---
+++ RickAndMortyApp/Models/Character.swift
@@ -0,0 +1,21 @@
+//
+//  Character.swift
+//  RickAndMortyApp
+//
+//  Created by MikhaiL on 10.10.2023.
+//
+
+import Foundation
+
+struct RickAndMorty: Decodable {
+    let results: [Character]
+}
+
+struct Character: Decodable {
+    let id: Int
+    let name: String
+    let status: String
+    let gender: String
+    let image: String
+    let episode: [URL]
+}
