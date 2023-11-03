8822398fe4b97d2846ef57556d0ede199b27d1db
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-11-03 16:42:34 +0300

    working with keychain

--- RickAndMortyApp/Services/AuthManager.swift
+++ RickAndMortyApp/Services/AuthManager.swift
@@ -13,5 +13,50 @@ final class AuthManager {
     
     private init() {}
     
-    // Keychain реализация
+    func save(username: String, password: String) {
+        let attributes: [String: Any] = [
+            kSecClass as String: kSecClassInternetPassword,
+            kSecAttrAccount as String: username,
+            kSecValueData as String: password.data(using: .utf8) as Any
+        ]
+        
+        let status = SecItemAdd(attributes as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            print("Не удалось сохранить данные в Keychain")
+            return
+        }
+        print("Данные успешно сохранены в Keychain")
+    }
+    
+    func update(password: String) {
+        let passwordData = password.data(using: .utf8)!
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+        ]
+        
+        let attributes: [String: Any] = [
+            kSecValueData as String: passwordData
+        ]
+        
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        guard status == errSecSuccess else {
+            print("Не удалось изменит пароль в Keychain")
+            return
+        }
+        print("Пароль успешно изменен!")
+    }
+    
+    func delete() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassInternetPassword
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess else {
+            print("Не удалось удалить данные из Keychain")
+            return
+        }
+        print("Данные успешно удалены из Keychain")
+    }
 }
